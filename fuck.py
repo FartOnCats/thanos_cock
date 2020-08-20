6309b6ce1b2ec352cc6191c55116c4f7624b0528
fartoncats <fartoncats@gmail.com>
2020-08-20 00:52:23 -0400

    mmm

--- fuck.py
+++ fuck.py
@@ -1,6 +1,6 @@
 
 
-import requests
+import requests,string,random
 
 def forumData():
     data = {
@@ -10,13 +10,20 @@ def forumData():
         'description': "lol",
     }
     return data
-
+def name():
+    return ''.join(random.choice(characters) for i in range(30))
 r = requests.Session()
 
 #login
 r.post('https://salty-reef-80979.herokuapp.com/login',data={'username':'admin','password':'admin'})
 
+#fill db
+characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+for i in range(5000):
+    requests.get('https://salty-reef-80979.herokuapp.com/register',data={'username': name(),'password': name()})
+    
+print("Done with users")
 #send meme
 for i in range(5000):
     x = r.post('https://salty-reef-80979.herokuapp.com/campgrounds',data=forumData())
-    print(x.status_code)
+print("Done")
